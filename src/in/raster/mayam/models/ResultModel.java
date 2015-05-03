01ccbf881ec3243f93b9ef42bf23ae80596a9387
marianocdiaz <marianocdiaz@gmail.com>
2015-05-03 00:03:37 +0000

    update

---
+++ src/in/raster/mayam/models/ResultModel.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.raster.mayam.models;
+
+/**
+ *
+ * @author mariano
+ */
+public class ResultModel {
+    
+    private String patientId;
+    
+    private String studyIUID;
+    
+    private String valueA;
+    
+    private String valueB;
+    
+    private String valueC;
+    
+    private String valueD;
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getStudyIUID() {
+        return studyIUID;
+    }
+
+    public void setStudyIUID(String studyIUID) {
+        this.studyIUID = studyIUID;
+    }
+
+    public String getValueA() {
+        return valueA;
+    }
+
+    public void setValueA(String valueA) {
+        this.valueA = valueA;
+    }
+
+    public String getValueB() {
+        return valueB;
+    }
+
+    public void setValueB(String valueB) {
+        this.valueB = valueB;
+    }
+
+    public String getValueC() {
+        return valueC;
+    }
+
+    public void setValueC(String valueC) {
+        this.valueC = valueC;
+    }
+
+    public String getValueD() {
+        return valueD;
+    }
+
+    public void setValueD(String valueD) {
+        this.valueD = valueD;
+    }
+    
+}
